c437b569765f17315ae3b0a690722389b9dbf38f
vikrampal <vpal3@uwo.ca>
2017-04-21 01:04:31 -0400

    Queue implementation of queue java

---
+++ src/set/JavaQueue.java
@@ -0,0 +1,36 @@
+package set;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaQueue {
+
+	public static void main(String[] args) {
+		
+		Queue<String> qs=new LinkedList<>();
+		qs.offer("rakesh");
+		qs.offer("Vicky");
+		qs.offer("yash");
+		qs.add("vikram");
+		qs.offer("Apporav");
+		
+		System.out.println(qs);
+		
+		System.out.println("isEmpty: "+ qs.isEmpty());
+		
+		System.out.println("peek in queue: "+ qs.peek());
+
+		System.out.println("pooling in queue: "+ qs.poll());
+		
+		
+		while(qs.peek()!=null){
+			System.out.println("isEmpty: "+ qs.peek());
+			qs.remove();
+			System.out.println(qs);
+			
+			
+		}
+
+	}
+
+}
